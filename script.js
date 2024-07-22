cf9c01a9eca1e3f16ef06d86a3817e1282a8e279
Benjamin <benjamin.4179@gmail.com>
2024-07-22 13:27:04 -0700

    chaching it up

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //connection check
 console.log("hello I am connected");
 
+<<<<<<< HEAD
 //declarrying my variables
 
 const hamburgerMenu = document.querySelector(".hamburger");
@@ -16,4 +17,13 @@ hamburgerMenu.addEventListener("click", () => {
 
   menu.style.display === "block" ? "none" : "block";
   console.log("click is going trough");
+=======
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburgerMenu = document.querySelector("hamburger");
+  const menu = document.querySelector("main-navigation-mobile-section");
+
+  hamburgerMenu.addEventListener("click", () => {
+    menu.style.display = menu.style.display = "block";
+  });
+>>>>>>> fad02cc (chaching it up)
 });
