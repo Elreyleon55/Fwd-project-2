bc82750ab30016fb1d555179371a23f333c1e990
Benjamin <benjamin.4179@gmail.com>
2024-07-22 15:14:27 -0700

    up to date with header branch

--- script.js
+++ script.js
@@ -1,33 +1,19 @@
+//connection check
+console.log("hello I am connected");
 
-//test
-console.log("I am connected");
-
-//getting variables
+//declarrying my variables
 
 const hamburgerMenu = document.querySelector(".hamburger");
+const menu = document.querySelector(".main-navigation-mobile-section");
 
-const dropdownmeu = document.querySelector(".main-navigation-mobile-section");
+menu.style.display = "block";
 
-//test
-console.log(dropdownmeu);
 console.log(hamburgerMenu);
 
-//setting defualt styles of dropdown
-
-dropdownmeu.style.display = "none";
-
-//getting on click on button and changing styles
-//&adding a class list to my hamburger menu to add a slide down animation in css
-
 hamburgerMenu.addEventListener("click", () => {
-  dropdownmeu.style.display =
-    dropdownmeu.style.display === "block" ? "none" : "block";
-  if (dropdownmeu.style.display === "block") {
-    dropdownmeu.classList.toggle("open");
-    console.log("the first one");
-  } else {
-    dropdownmeu.classList.toggle("open");
-    console.log("the second one");
+  if (menu.style.display === "style") {
   }
-  console.log(dropdownmeu);
+
+  menu.style.display === "block" ? "none" : "block";
+  console.log("click is going trough");
 });
