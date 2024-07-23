d625d366d25eb9d7d872286b9dfec2f4bc22ea18
Benjamin <benjamin.4179@gmail.com>
2024-07-23 13:08:35 -0700

    added media query

--- script.js
+++ script.js
@@ -16,8 +16,17 @@ console.log(hamburgerMenu);
 dropdownmeu.style.display = "none";
 
 //getting on click on button and changing styles
+//&adding a class list to my hamburger menu to add a slide down animation in css
 
 hamburgerMenu.addEventListener("click", () => {
   dropdownmeu.style.display =
     dropdownmeu.style.display === "block" ? "none" : "block";
+  if (dropdownmeu.style.display === "block") {
+    dropdownmeu.classList.toggle("open");
+    console.log("the first one");
+  } else {
+    dropdownmeu.classList.toggle("open");
+    console.log("the second one");
+  }
+  console.log(dropdownmeu);
 });
