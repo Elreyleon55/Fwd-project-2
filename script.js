fed859c5b250192977ea034115cd5f60cd19788d
Benjamin <benjamin.4179@gmail.com>
2024-07-23 14:10:57 -0700

    javascript update

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-
 console.log("I am connected");
-
-
-
 
 const hamburgerMenu = document.querySelector(".hamburger");
 const menu = document.querySelector(".main-navigation-mobile-section");
@@ -12,6 +8,15 @@ menu.style.display = "block";
 console.log(hamburgerMenu);
 
 hamburgerMenu.addEventListener("click", () => {
-  menu.style.display === "block" ? "none" : "block";
-  console.log("click is going trough");
+  dropdownmeu.style.display =
+    dropdownmeu.style.display === "block" ? "none" : "block";
+  if (dropdownmeu.style.display === "block") {
+    dropdownmeu.classList.toggle("open");
+    console.log("the first one");
+  } else {
+    dropdownmeu.classList.toggle("open");
+    console.log("the second one");
+  }
+  console.log(dropdownmeu);
+  console.log("new save");
 });
